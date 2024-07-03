66059de8cb99b8aeb13f768f79378025375c11d2
obigvee <adedejivictor98@gmail.com>
2024-07-03 09:27:42 +0100

    :simple web server with greet endpoint:

---
+++ go.mod
@@ -0,0 +1,3 @@
+module obigvee.com/webServer01
+
+go 1.21.1
---
+++ server.go
@@ -0,0 +1,57 @@
+package main
+
+import(
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request)  {
+	// get visitor anem
+	vName := r.URL.Query().Get("visitor_name")
+	if vName == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Missing vistor_name parameter")
+	}
+
+	// get client IP addres
+	clientIP, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		clientIP = "Unkown"
+	}
+
+	// simulate location
+	location := "Ebonyi"
+
+	greeting  := fmt.Sprintf("Hello, %s! The temperature is 11 degress Celcius in %s", vName, location)
+	responseData := map[string]string{
+		"client_ip": clientIP,
+		"location": location,
+		"greeting": greeting,
+	}
+
+	// Encoding response data to JSON
+	responseJSON, err := json.Marshal(responseData)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "Error creating responses: %v", err)
+		return
+	}
+	// Write JSON response to client
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseJSON)
+}
+
+func main ()  {
+	// define the handler func for th api/hello route
+	http.HandleFunc("/api/hello", handler)
+	
+	// start the server on port 8080
+	fmt.Println("Server listening on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
